0e68fe8cee92011a708e5ed9ea9b897d56f35aa1
anfefuro <andres.fuentes@unillanos.edu.co>
2021-12-23 12:04:27 -0500

    I changed: onSubmit, useEffect, lunch, consts, titles, christmas, inputs boxes, money values.

--- src/App.jsx
+++ src/App.jsx
@@ -1,162 +1,171 @@
 import React, { useState, useEffect } from 'react'
 import { useForm } from 'react-hook-form';
 
+//-QUITAR ON SUBMIT (done)
+//-PROBAR QUITANDO LOS VALORES RESET DE LOS USEEFFECT MAS LARGOS (done)
+//-LUNCH (done)
+//-CONSTANTES (done)
+//-CUANDO CAMBIE EL SEMESTRE QWUE TAMBIEN CAMBIE EL TITULO (done)
+//-VALIDACION PARA TERCER TITULO (done)
+//-QUE DESAPAREZCA NAVIDAD (done) 
+//-NUMEROS CON PUNTOS Y COMAS (done)
+//-HACER LOS INPUTS MAS GRANDES (done)
+
 const App = () => {
 
-    const {register, watch, handleSubmit} = useForm ();
+    const options2 = { style: 'currency', currency: 'USD' };
+    const numberFormat2 = new Intl.NumberFormat('en-US', options2);
+    const date = new Date();
+    const christmas = 548696;
+
+    const {register, watch} = useForm ();
 
     const [salaryBase, setSalaryBase] = useState(0);
     const [sumPayForExtraHours, setSumPayForExtraHours] = useState(0);
     const [payForBonusRecess, setPayForBonusRecess] = useState(0);
-
-    const [lounchAllowance, setLounchAllowance] = useState(0);
+    const [lunchAllowance, setLunchAllowance] = useState(0);
     const [transAllowance, setTransAllowance] = useState(0);
     const [extraHoursForSem, setExtraHoursForSem] = useState(0);
     const [recessBonusPayForSem, setRecessBonusPayForSem] = useState(0);
-
     const [totalSemestralBonus, setTotalSemestralBonus] = useState(0);
     const [totalServicesBonus, setTotalServicesBonus] = useState(0);
     const [totalValueForBonusConcept, setTotalValueForBonusConcept] = useState(0);
-
+    const [semestralChoice, setSemestralChoice] = useState(0);
     const [christmasBonus, setChristmasBonus] = useState(0);
     const [profitSharing, setProfitSharing] = useState(0);
-
-    const [semestralChoice, setSemestralChoice] = useState (0);
 
     const salary = (event) => {
         setSalaryBase(event.target.value);
     }
-
     const extraHours = (event) => {
         setSumPayForExtraHours(event.target.value);
     }
-
     const payForBonus = (event) => {
         setPayForBonusRecess(event.target.value);
     }
 
-    const christmas = 548696;
-
-    //Second semestre: true - First semestre: false.
+    //Second semestre: [semestralChoice(true)] - First semestre: [semestralChoice(false)].
     useEffect(()=>{
         salaryBase > 0 ?
-            setSemestralChoice(true) :
+            setSemestralChoice(true) : 
             setSemestralChoice(0)
 
         salaryBase > 0 ? 
-            setLounchAllowance(129828) :
-            setLounchAllowance(0)
+            setLunchAllowance(129828) :
+            setLunchAllowance(0)
 
         salaryBase <= 2725578 && salaryBase > 0 ? 
             setTransAllowance(106454) : 
             setTransAllowance(0)
     }, [salaryBase]) 
-    //______________________________________________
 
     useEffect(()=>{
         sumPayForExtraHours > 0 ?
-            setExtraHoursForSem(parseInt(parseInt(sumPayForExtraHours) / 6)) :
+            setExtraHoursForSem(parseFloat(parseFloat(sumPayForExtraHours) / 6)) :
             setExtraHoursForSem(0)
     }, [sumPayForExtraHours])
 
     useEffect(() => {
         payForBonusRecess > 0 ?
-            setRecessBonusPayForSem(parseInt(parseInt(payForBonusRecess) / 6)) :
+            setRecessBonusPayForSem(parseFloat(parseFloat(payForBonusRecess) / 6)) :
             setRecessBonusPayForSem(0)
     }, [payForBonusRecess])
 
     useEffect(() => {
         totalSemestralBonus > 0 ?
-            setTotalServicesBonus(parseInt(parseInt(totalSemestralBonus)*0.583333)):
+            setTotalServicesBonus(parseFloat(parseFloat(totalSemestralBonus)*0.583333)):
             setTotalServicesBonus(0)
     },[totalSemestralBonus])
 
-    //_______________________________________________
-
     useEffect(() => {
-        semestralChoice === true ?
-            setChristmasBonus(parseInt((parseInt(christmas*1.35))+((parseInt(salaryBase)-(parseInt(christmas*2)))*0.3))) :
+        semestralChoice ?
+            setChristmasBonus(parseFloat((parseInt(christmas*1.35))+((parseFloat(salaryBase)-(parseFloat(christmas*2)))*0.3))) :
             setChristmasBonus(0)   
-    }, [semestralChoice, christmas, salaryBase])
+    }, [semestralChoice, salaryBase])
 
     useEffect(() => {
-        christmasBonus === 0 ?
+        !christmasBonus ?
             setProfitSharing(298106) :
             setProfitSharing(0)
     }, [christmasBonus])
-    //________________________________________________
-
+    
     useEffect(() => {    
         salaryBase > 0 ?
-            setTotalSemestralBonus(parseInt(lounchAllowance+transAllowance+extraHoursForSem+recessBonusPayForSem+(parseInt(christmasBonus)/6))+parseInt(salaryBase)+(parseInt(profitSharing))) :
+            setTotalSemestralBonus(parseFloat(lunchAllowance+transAllowance+extraHoursForSem+recessBonusPayForSem+(parseFloat(christmasBonus)/6))+parseFloat(salaryBase)+(parseFloat(profitSharing))) :
             setTotalSemestralBonus(0)
 
         salaryBase > 0 ?
-            setTotalValueForBonusConcept(parseInt(totalServicesBonus+christmasBonus)+parseInt(totalSemestralBonus)) :
+            setTotalValueForBonusConcept(parseFloat(totalServicesBonus+christmasBonus)+parseFloat(totalSemestralBonus)) :
             setTotalValueForBonusConcept(0)
 
-    }, [semestralChoice, lounchAllowance, transAllowance, extraHoursForSem, recessBonusPayForSem, christmasBonus, salaryBase, profitSharing, christmas, totalServicesBonus, totalSemestralBonus])
-    //___________________________________________________
-
+    }, [semestralChoice, lunchAllowance, transAllowance, extraHoursForSem, recessBonusPayForSem, christmasBonus, salaryBase, profitSharing, totalServicesBonus, totalSemestralBonus])
+    
     //Verification (React Hook Form)
-
-    console.log(watch(["firstBox", "secondBox", "thirdBox"]))
-
     const firstBox = watch("firstBox");
     const secondBox = watch("secondBox");
     const thirdBox = watch("thirdBox");
 
     return (
-        <div className="container mx-auto w-3/5 border mb-5 rounded">
-            <h1 className="text-2xl p-5 text-center border text-gray-900 bg-gray-200 font-bold">CALCULO PRIMAS SEMESTRALES SEGUNDO SEMESTRE 2021</h1>
+        <div className="container mx-auto w-3/5 border mb-5 rounded bg-white">
+            <h1 className="text-2xl p-5 text-center border text-gray-900 bg-gray-200 font-bold">CALCULO PRIMAS SEMESTRALES {date.getMonth() < 6 ? "PRIMER SEMESTRE" : "SEGUNDO SEMESTRE"} {date.getFullYear()}</h1>
             <div className="flex flex-wrap">
-                <form onSubmit={handleSubmit ((data) => console.log(data))} className="w-full sm:w-1/2 text-center p-5">
-                    <label className="text-lg font-bold pt-5 m-2 text-center text-gray-600" htmlFor="firstBox">Sueldo básico a Noviembre 30 (*)</label>
+                <div className="w-full sm:w-1/2 text-center p-5">
+                    <label className="text-xl font-bold pt-5 m-2 text-center text-gray-600" htmlFor="firstBox">Sueldo básico a Noviembre 30 (*)</label>
                     <br></br>
-                    <input {...register("firstBox")} onKeyUp={salary} className="border shadow m-2 w-full rounded leading-tight focus:outline-none focus:shadow-outline" type="number" placeholder="0" name="firstBox"/> 
-                    <p className="text-red-500 text-xs italic">{firstBox == false ? "Ingrese un valor numerico" : ""}</p>
+                    <input {...register("firstBox")} onKeyUp={salary} className="py-2 text-xl border shadow m-2 w-9/12 rounded leading-tight focus:outline-none focus:border-gray-500 focus:ring-1 focus:ring-gray-500" type="number" placeholder="0" name="firstBox"/> 
+                    <p className="text-red-500 text-xs italic">{!firstBox ? "Ingrese un valor numerico" : ""}</p>
+                    <h1 className="text-gray-200 text-center">____________________</h1>
                     <br></br>
 
                     <h1 className="font-bold text-2xl pt-5 text-gray-600">VALORES PROPORCIONALES</h1>
                     <br></br>
 
-                    <label className="text-lg font-bold pt-5 m-2 text-center text-gray-600" htmlFor="secondBox">Suma de lo pagado por horas extras en el semestre</label>
+                    <label className="text-xl font-bold pt-5 m-2 text-center text-gray-600" htmlFor="secondBox">Suma de lo pagado por horas extras en el semestre</label>
                     <br></br>
-                    <input {...register("secondBox")} onKeyUp={extraHours} className="shadow border m-2 w-full rounded leading-tight focus:outline-none focus:shadow-outline" type="number" placeholder="0"/> 
-                    <p className="text-red-500 text-xs italic">{secondBox == false ? "Ingrese un valor numerico" : ""}</p>
+                    <input {...register("secondBox")} onKeyUp={extraHours} className="text-xl py-2 shadow border m-2 w-9/12 rounded leading-tight focus:outline-none focus:border-gray-500 focus:ring-1 focus:ring-gray-500" type="number" placeholder="0"/> 
+                    <p className="text-red-500 text-xs italic">{!secondBox ? "Ingrese un valor numerico" : ""}</p>
+                    <h1 className="text-gray-200 text-center">____________________</h1>
                     <br></br>
                     <br></br>
 
-                    <label className="text-lg font-bold pt-5 m-2 text-center text-gray-600" htmlFor="thirdBox">Valor pagado por prima de vacaciones en el semestre
-                    (JULIO 1 A DICIEMBRE 31)</label>
+                    <label className="text-xl font-bold pt-5 m-2 text-center text-gray-600" htmlFor="thirdBox">Valor pagado por prima de vacaciones en el semestre {date.getMonth() < 6 ? "(ENERO 1 A JUNIO 30)" : "(JULIO 1 A DICIEMBRE 31)"} </label>
                     <br></br>
-                    <input {...register("thirdBox")} onKeyUp={payForBonus} className="shadow border m-2 w-full rounded leading-tight focus:outline-none focus:shadow-outline" type="number" placeholder="0"/> 
-                    <p className="text-red-500 text-xs italic">{thirdBox == false ? "Ingrese un valor numerico" : ""}</p>
-                </form>
+                    <input {...register("thirdBox")} onKeyUp={payForBonus} className="text-xl py-2 shadow border m-2 w-9/12 rounded leading-tight focus:outline-none focus:border-gray-500 focus:ring-1 focus:ring-gray-500" type="number" placeholder="0"/> 
+                    <p className="text-red-500 text-xs italic">{!thirdBox ? "Ingrese un valor numerico" : ""}</p>
+                    <h1 className="text-gray-200 text-center">____________________</h1>
+                </div>
                 <div className="w-full sm:w-1/2 pb-5 px-3">
-                    <h1 className="text-lg font-bold pt-2 m-2 text-center text-gray-600">Subsidio de almuerzo con efecto prestacional</h1>
-                    <h1 className="text-2xl font-bold text-center">${lounchAllowance}</h1>
+                    <h1 className="text-xl font-bold pt-2 m-2 text-center text-gray-600">Subsidio de almuerzo con efecto prestacional</h1>
+                    <h1 className="text-2xl font-bold text-center">{numberFormat2.format(lunchAllowance)}</h1>
+                    <h1 className="text-gray-200 text-center">____________________</h1>
                     
-                    <h1 className="text-lg font-bold pt-5 m-2 text-center text-gray-600">Subsidio de transporte</h1>
-                    <h1 className="text-2xl font-bold text-center">${transAllowance}</h1>
+                    <h1 className="text-xl font-bold pt-5 m-2 text-center text-gray-600">Subsidio de transporte</h1>
+                    <h1 className="text-2xl font-bold text-center">{numberFormat2.format(transAllowance)}</h1>
+                    <h1 className="text-gray-200 text-center">____________________</h1>
                     
-                    <h1 className="text-lg font-bold pt-5 m-2 text-center text-gray-600">Horas extras en el semestre</h1>
-                    <h1 className="text-2xl font-bold text-center">${extraHoursForSem}</h1>
+                    <h1 className="text-xl font-bold pt-5 m-2 text-center text-gray-600">Horas extras en el semestre</h1>
+                    <h1 className="text-2xl font-bold text-center">{numberFormat2.format(extraHoursForSem)}</h1>
+                    <h1 className="text-gray-200 text-center">____________________</h1>
                     
-                    <h1 className="text-lg font-bold pt-5 m-2 text-center text-gray-600">Prima de vacaciones pagadas en el semestre</h1>
-                    <h1 className="text-2xl font-bold text-center">${recessBonusPayForSem}</h1>
+                    <h1 className="text-xl font-bold pt-5 m-2 text-center text-gray-600">Prima de vacaciones pagadas en el semestre</h1>
+                    <h1 className="text-2xl font-bold text-center">{numberFormat2.format(recessBonusPayForSem)}</h1>
+                    <h1 className="text-gray-200 text-center">____________________</h1>
                     
-                    <h1 className="text-lg font-bold pt-5 m-2 text-center text-gray-600">TOTAL PRIMA SEMESTRAL</h1>
-                    <h1 className="text-2xl font-bold text-center">${totalSemestralBonus}</h1>
+                    <h1 className="text-xl font-bold pt-5 m-2 text-center text-gray-600">TOTAL PRIMA SEMESTRAL</h1>
+                    <h1 className="text-2xl font-bold text-center">{numberFormat2.format(totalSemestralBonus)}</h1>
+                    <h1 className="text-gray-200 text-center">____________________</h1>
                     
-                    <h1 className="text-lg font-bold pt-5 m-2 text-center text-gray-600">TOTAL PRIMA SERVICIOS</h1>
-                    <h1 className="text-2xl font-bold text-center">${totalServicesBonus}</h1>
+                    <h1 className="text-xl font-bold pt-5 m-2 text-center text-gray-600">TOTAL PRIMA SERVICIOS</h1>
+                    <h1 className="text-2xl font-bold text-center">{numberFormat2.format(totalServicesBonus)}</h1>
+                    <h1 className="text-gray-200 text-center">____________________</h1>
                     
-                    <h1 className="text-lg font-bold pt-5 m-2 text-center text-gray-600">BONIFICACION DE NAVIDAD</h1>
-                    <h1 className="text-2xl font-bold text-center">${christmasBonus}</h1>
+                    <h1 className="text-xl font-bold pt-5 m-2 text-center text-gray-600">{date.getMonth() > 6 ? "BONIFICACION DE NAVIDAD" : false}</h1>
+                    <h1 className="text-2xl font-bold text-center">{date.getMonth() > 6 ? numberFormat2.format(christmasBonus) : false}</h1>
+                    <h1 className="text-gray-200 text-center">____________________</h1>
                     
-                    <h1 className="text-lg font-bold pt-5 m-2 text-center text-gray-600">VALOR TOTAL POR CONCEPTO DE PRIMAS</h1>
-                    <h1 className="text-2xl font-bold text-center">${totalValueForBonusConcept}</h1>
+                    <h1 className="text-xl font-bold pt-5 m-2 text-center text-gray-600">VALOR TOTAL POR CONCEPTO DE PRIMAS</h1>
+                    <h1 className="text-2xl font-bold text-center">{numberFormat2.format(totalValueForBonusConcept)}</h1>
+                    <h1 className="text-gray-200 text-center">____________________</h1>
                 </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily: {
-    'sans': ['Helvetica', 'Arial', 'sans-serif'],
-  },
-    extend: {},
+    extend: {
+      fontFamily{
+        sans: "sans, serif",
+      }
+    },
   },
   variants: {
     extend: {},
